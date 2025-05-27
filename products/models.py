c56491c6f8f9731b60444f045d830840dc27b361
Alex <alexwallax@gmail.com>
2025-05-27 03:50:35 -0300

    tabela em products

--- products/models.py
+++ products/models.py
@@ -7,3 +7,6 @@ class Product(models.Model):
     brand = models.CharField(max_length=100) # marca
     description = models.TextField(null=True, blank=True) # descrição
     price = models.DecimalField(max_digits=10, decimal_places=2) # preço
+
+    def __str__(self):
+        return str(self.title)
