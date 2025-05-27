96b2cfcf3c41b085f1106efbd1b8c02ca94cd9cd
Alex <alexwallax@gmail.com>
2025-05-27 00:49:17 -0300

    products models

--- products/models.py
+++ products/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    title = models.CharField(max_length=200) # titulo
+    category = models.CharField(max_length=100) # categoria
+    brand = models.CharField(max_length=100) # marca
+    description = models.TextField(null=True, blank=True) # descrição
+    price = models.DecimalField(max_digits=10, decimal_places=2) # preço
