cb49c2a2d417e8ae1f544cfe719330b05440bf87
Alex <alexwallax@gmail.com>
2025-05-30 21:50:53 -0300

    templates

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'brand', 'price') # campo de lista
+    search_fields = ('title', ) # campo de busca por titulo     
+    list_filter = ('category', 'brand') # listar os filtros
+
+admin.site.register(models.Product, ProductAdmin)
--- products/views.py
+++ products/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from products.models import Product #importando a tabela de produtos.
 
-# Create your views here.
+
+def product_view(request):
+    products = Product.objects.all() # SELECT * FROM PRODUCTS - no banco de dados
+    
+    return render(
+        request=request,
+        template_name='products.html',
+        context={'products': products} # envia os dados do DB para o template
+    )
+
+
+
